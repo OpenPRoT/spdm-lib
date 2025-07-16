adc082b7ef37a8572bb4d4c05fefd7afcf53c001
Will Marone <Will.Marone@amd.com>
2025-07-16 08:52:41 -0700

    Introductory commit
    This introduces the sources from https://github.com/chipsalliance/caliptra-mcu-sw
    for spdm-lib into a stand-alone repo.
    
    This contains *no* changes to the sources and serves as an introductory point for
    upstream changes to the tree without immediately involving OpenPRoT changes.

---
+++ src/error.rs
@@ -0,0 +1,44 @@
+// Licensed under the Apache-2.0 license
+
+// use crate::cert_mgr::DeviceCertsMgrError;
+use crate::cert_store::CertStoreError;
+use crate::chunk_ctx::ChunkError;
+use crate::codec::CodecError;
+use crate::commands::error_rsp::ErrorCode;
+use crate::measurements::common::MeasurementsError;
+use crate::protocol::SignCtxError;
+use crate::transcript::TranscriptError;
+use crate::transport::TransportError;
+use libapi_caliptra::error::CaliptraApiError;
+
+#[derive(Debug)]
+pub enum SpdmError {
+    UnsupportedVersion,
+    InvalidParam,
+    Codec(CodecError),
+    Transport(TransportError),
+    Command(CommandError),
+    BufferTooSmall,
+    UnsupportedRequest,
+    CertStore(CertStoreError),
+    CaliptraApi(CaliptraApiError),
+}
+
+pub type SpdmResult<T> = Result<T, SpdmError>;
+
+pub type CommandResult<T> = Result<T, (bool, CommandError)>;
+
+#[derive(Debug, PartialEq)]
+pub enum CommandError {
+    BufferTooSmall,
+    Codec(CodecError),
+    ErrorCode(ErrorCode),
+    UnsupportedRequest,
+    SignCtx(SignCtxError),
+    InvalidChunkContext,
+    Chunk(ChunkError),
+    CertStore(CertStoreError),
+    CaliptraApi(CaliptraApiError),
+    Transcript(TranscriptError),
+    Measurement(MeasurementsError),
+}
