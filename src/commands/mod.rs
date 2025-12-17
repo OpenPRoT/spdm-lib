49e9b7c29d5e6e3d2f517e35ebd8e9e0bc202f5f
Marvin Gudel <marvin.gudel@9elements.com>
2025-12-17 16:25:30 +0100

    Refactor Capabilities command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,7 @@
 // Licensed under the Apache-2.0 license
 
 pub mod algorithms_rsp;
-pub mod capabilities_rsp;
+pub mod capabilities;
 pub mod certificate_rsp;
 pub mod challenge_auth_rsp;
 pub mod chunk_get_rsp;
