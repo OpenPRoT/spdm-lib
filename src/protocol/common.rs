d8cf2adcab25b511b1128a3ea75a80ea40128638
Marvin Gudel <marvin.gudel@9elements.com>
2025-12-18 11:11:36 +0100

    Fix clippy warning for is_response check

--- src/protocol/common.rs
+++ src/protocol/common.rs
@@ -76,7 +76,7 @@ impl ReqRespCode {
         Ok(context)
     }
     pub(crate) fn is_response(&self) -> bool {
-        *self as u8 >= 0x00 && *self as u8 <= 0x7F
+        *self as u8 <= 0x7F
     }
     pub(crate) fn is_request(&self) -> bool {
         !self.is_response()
